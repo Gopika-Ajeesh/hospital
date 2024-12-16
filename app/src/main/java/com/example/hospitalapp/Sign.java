2c6cffa57be3924cf15819cce1620c6b131458f2
Gopika-Ajeesh <cgopika1234@gmail.com>
2024-12-17 01:21:41 +0530

    yes

---
+++ app/src/main/java/com/example/hospitalapp/Sign.java
@@ -0,0 +1,23 @@
+package com.example.hospitalapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Sign extends AppCompatActivity {
+    EditText et1,et2,et3,et4,et5,et6,et7;
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign);
+
+    }
+}
